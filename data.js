9924ee13876bce8c2e0c4b0c6baa8a3246efc9ed
amaaninteriorsvly-code <amaaninteriorsvly@gmail.com>
2026-07-13 16:58:45 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,219 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-view--1",
+      "name": "VIEW -1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7010554361872288,
+          "pitch": 0.8121718778347375,
+          "rotation": 6.283185307179586,
+          "target": "1-view-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-view-2",
+      "name": "VIEW-2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2014779735386334,
+          "pitch": 0.7426870696231873,
+          "rotation": 0,
+          "target": "2-view-3"
+        },
+        {
+          "yaw": 2.3402112327480564,
+          "pitch": 0.8777667145186641,
+          "rotation": 7.853981633974483,
+          "target": "0-view--1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-view-3",
+      "name": "VIEW-3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.038934267929105815,
+        "pitch": 0.003759513653534441,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.05228705605227901,
+          "pitch": 0.5438131310739998,
+          "rotation": 0,
+          "target": "3-view-4"
+        },
+        {
+          "yaw": 0.2959647917321533,
+          "pitch": 0.6310642648324354,
+          "rotation": 1.5707963267948966,
+          "target": "4-view-5"
+        },
+        {
+          "yaw": -2.966219458748057,
+          "pitch": 1.0260462692218884,
+          "rotation": 0,
+          "target": "1-view-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-view-4",
+      "name": "VIEW-4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3782561403636997,
+          "pitch": 1.4137189376137886,
+          "rotation": 5.497787143782138,
+          "target": "2-view-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-view-5",
+      "name": "VIEW-5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2366906396275876,
+          "pitch": 1.1410651083542689,
+          "rotation": 0,
+          "target": "2-view-3"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "FARSHAD INTERIOR",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
